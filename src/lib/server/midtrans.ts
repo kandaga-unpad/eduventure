843543791d7e06fc41f45073718035236b5c3e55
chrisnaadhi <chrisnaadhip@gmail.com>
2025-01-19 15:29:00 +0700

    Update midtrans.ts

--- src/lib/server/midtrans.ts
+++ src/lib/server/midtrans.ts
@@ -6,13 +6,13 @@ import { PRIVATE_MIDTRANS_SERVERKEY } from '$env/static/private';
 import { PRIVATE_MIDTRANS_CLIENTKEY } from '$env/static/private';
 
 export const midtransCoreApi = new midtransClient.CoreApi({
-  isProduction: true,
+  isProduction: false,
   serverKey: PRIVATE_MIDTRANS_SERVERKEY,
   clientKey: PRIVATE_MIDTRANS_CLIENTKEY,
 })
 
 export const midTransSnap = new midtransClient.Snap({
-  isProduction: true,
+  isProduction: false,
   serverKey: PRIVATE_MIDTRANS_SERVERKEY,
   clientKey: PRIVATE_MIDTRANS_CLIENTKEY,
 })
