6dde6f5824e5df8f066dd3093c10748bfa6e43bc
chrisnaadhi <chrisnaadhip@gmail.com>
2025-01-19 16:43:08 +0700

    Update midtrans.ts

--- src/lib/server/midtrans.ts
+++ src/lib/server/midtrans.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import midtransClient from 'midtrans-client';
-// import { PRIVATE_KANDAGA_MIDTRANS_MERCHANTID } from '$env/static/private';
-import { PRIVATE_KANDAGA_MIDTRANS_SERVERKEY } from '$env/static/private';
-import { PRIVATE_KANDAGA_MIDTRANS_CLIENTKEY } from '$env/static/private';
+// import { PRIVATE_EDUVENTURE_MIDTRANS_MERCHANTID } from '$env/static/private';
+import { PRIVATE_EDUVENTURE_MIDTRANS_SERVERKEY } from '$env/static/private';
+import { PRIVATE_EDUVENTURE_MIDTRANS_CLIENTKEY } from '$env/static/private';
 
 export const midtransCoreApi = new midtransClient.CoreApi({
   isProduction: false,
-  serverKey: PRIVATE_KANDAGA_MIDTRANS_SERVERKEY,
-  clientKey: PRIVATE_KANDAGA_MIDTRANS_CLIENTKEY,
+  serverKey: PRIVATE_EDUVENTURE_MIDTRANS_SERVERKEY,
+  clientKey: PRIVATE_EDUVENTURE_MIDTRANS_CLIENTKEY,
 })
 
 export const midTransSnap = new midtransClient.Snap({
   isProduction: false,
-  serverKey: PRIVATE_KANDAGA_MIDTRANS_SERVERKEY,
-  clientKey: PRIVATE_KANDAGA_MIDTRANS_CLIENTKEY,
+  serverKey: PRIVATE_EDUVENTURE_MIDTRANS_SERVERKEY,
+  clientKey: PRIVATE_EDUVENTURE_MIDTRANS_CLIENTKEY,
 })
