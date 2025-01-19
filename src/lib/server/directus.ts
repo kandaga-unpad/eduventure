8cfd25eebd35acd4ccdbdfdf09b91d9b8f09cdd4
chrisnaadhi <chrisnaadhip@gmail.com>
2025-01-19 16:44:20 +0700

    Update directus.ts

--- src/lib/server/directus.ts
+++ src/lib/server/directus.ts
@@ -1,12 +1,12 @@
 import { createDirectus, rest, staticToken } from '@directus/sdk';
-import { PUBLIC_APIURL } from '$env/static/public';
-import { PRIVATE_EDUVENTURE__APITOKEN } from '$env/static/private';
+import { PUBLIC_EDUVENTURE_APIURL } from '$env/static/public';
+import { PRIVATE_EDUVENTURE_APITOKEN } from '$env/static/private';
 
 function getDirectusInstance(fetch?: typeof globalThis.fetch) {
   const options = fetch ? { globals: { fetch } } : {};
-  const directus = createDirectus(PUBLIC_APIURL, options)
+  const directus = createDirectus(PUBLIC_EDUVENTURE_APIURL, options)
     .with(rest())
-    .with(staticToken(PRIVATE_EDUVENTURE__APITOKEN));
+    .with(staticToken(PRIVATE_EDUVENTURE_APITOKEN));
 
   return directus;
 }
