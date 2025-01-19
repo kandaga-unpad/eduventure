13f73efe648cbe76fc5aa9f3e4870f6a0a755119
chrisnaadhi <chrisnaadhip@gmail.com>
2025-01-19 16:34:08 +0700

    fix env

--- src/auth.ts
+++ src/auth.ts
@@ -1,21 +1,21 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
-import { PRIVATE_GOOGLE_CLIENTID, PRIVATE_GOOGLE_SECRET, PRIVATE_AUTH_SECRET } from "$env/static/private";
-import { PUBLIC_HOST } from "$env/static/public";
+import { PRIVATE_EDUVENTURE_GOOGLE_CLIENTID, PRIVATE_EDUVENTURE_GOOGLE_SECRET, PRIVATE_EDUVENTURE_AUTH_SECRET } from "$env/static/private";
+import { PUBLIC_EDUVENTURE_HOST } from "$env/static/public";
 import Google from "@auth/sveltekit/providers/google"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [Google({
-    clientId: PRIVATE_GOOGLE_CLIENTID,
-    clientSecret: PRIVATE_GOOGLE_SECRET,
+    clientId: PRIVATE_EDUVENTURE_GOOGLE_CLIENTID,
+    clientSecret: PRIVATE_EDUVENTURE_GOOGLE_SECRET,
   })],
-  secret: PRIVATE_AUTH_SECRET,
+  secret: PRIVATE_EDUVENTURE_AUTH_SECRET,
   callbacks: {
     async signIn({ user }) {
-      const existingUser = await fetch(`${PUBLIC_HOST}/api/user/` + user.email)
+      const existingUser = await fetch(`${PUBLIC_EDUVENTURE_HOST}/api/user/` + user.email)
       const response = await existingUser.json();
 
       if (response.user.length === 0) {
-        await fetch(`${PUBLIC_HOST}/api/user`, {
+        await fetch(`${PUBLIC_EDUVENTURE_HOST}/api/user`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
--- src/lib/server/directus.ts
+++ src/lib/server/directus.ts
@@ -1,12 +1,12 @@
 import { createDirectus, rest, staticToken } from '@directus/sdk';
 import { PUBLIC_APIURL } from '$env/static/public';
-import { PRIVATE_APITOKEN } from '$env/static/private';
+import { PRIVATE_EDUVENTURE__APITOKEN } from '$env/static/private';
 
 function getDirectusInstance(fetch?: typeof globalThis.fetch) {
   const options = fetch ? { globals: { fetch } } : {};
   const directus = createDirectus(PUBLIC_APIURL, options)
     .with(rest())
-    .with(staticToken(PRIVATE_APITOKEN));
+    .with(staticToken(PRIVATE_EDUVENTURE__APITOKEN));
 
   return directus;
 }
