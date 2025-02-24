6e13e6db5a161aaa6e678da753f7c2e583fcd520
chrisnaadhi <chrisnaadhip@gmail.com>
2025-02-24 21:45:36 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,12 @@
 	const eduventureExperience = {
 		color: 'green',
 		nama: 'Eduventure Experience',
-		harga: 'Rp350.000',
-		detail: 'Kaos Unpad + Snackbox',
+		harga: 'Sold Out',
+		detail: 'SEE YOU MAYBE NEXT TIME?!',
 		textPertama:
 			'Pematerian umum dipandu oleh Kantor Komunikasi Publik dengan durasi 30 menit di Gedung Rektorat. Kegiatan dimulai dari Gedung Rektorat dan dilanjutkan ke fakultas/sekolah menggunakan angkutan dalam kampus yang dipandu oleh panitia mahasiswa magang.',
 		textKedua:
-			'Untuk Eduventure Experience tanggal 22-23 Februari 2025, tiket sudah termasuk akses kegiatan Open House Universitas Padjadjaran',
+			'Terima Kasih kepada seluruh Peserta Eduventure Experience yang telah mengikuti Eduventure pada 22-23 Februari 2025. Kami tunggu kehadiran kalian di Eduventure Experience berikutnya!',
 		select: 'Pilih Experience',
 		url: '/eduventure/experience'
 	};
